8bbcdf5a8d6d9d07070689018864ba7ae9458057
Kanana-Mwenda <Lizamwenda95@gmail.com>
2024-12-20 22:02:37 +0300

    commit

--- script.js
+++ script.js
@@ -13,7 +13,6 @@
                         <img src="${product.image}" class="card-img-top" alt="${product.name}">
                         <div class="card-body">
                             <h5 class="card-title">${product.name}</h5>
-                            <p class="card-text">${product.description}</p>
                             <p class="card-text">Price: $${product.price}</p>
                             <button class="btn btn-primary add-to-cart" data-id="${product.id}">Add to Cart</button>
                         </div>
